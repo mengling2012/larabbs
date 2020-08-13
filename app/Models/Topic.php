bc0ff74c8b2962249a270586bcad0c667874413a
admin <kunyun.wu@rklink.cn>
2020-08-13 14:10:54 +0000

    用户可以创建话题

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,8 +5,7 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = [
-        'title', 'body', 'user_id', 'category_id', 'reply_count',
-        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug',
+        'title', 'body', 'category_id', 'excerpt', 'slug'
     ];
 
     public function category()
@@ -45,5 +44,5 @@ class Topic extends Model
         // 按照创建时间排序
         return $query->orderBy('created_at', 'desc');
     }
-    
+
 }
