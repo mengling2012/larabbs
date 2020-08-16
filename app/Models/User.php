01cf9c79cb539d85b46dbeb38c52a5f686df28df
admin <kunyun.wu@rklink.cn>
2020-08-16 05:20:38 +0000

    集成 laravel-permission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
+use Spatie\Permission\Traits\HasRoles;
+
 use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
@@ -16,6 +18,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     use Notifiable {
         notify as protected laravelNotify;
     }
+
+    use HasRoles;
+
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
@@ -64,5 +69,5 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
-    
+
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
+
 class UsersTableSeeder extends Seeder
 {
     public function run()
@@ -15,6 +16,17 @@ class UsersTableSeeder extends Seeder
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
+
+        
         $user->save();
+
+        
     }
 }
